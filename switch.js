93d079a98f71a341279b2b85a910925c7ce19e3a
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-04-27 16:39:27 +0100

    player site responsiveness

--- switch.js
+++ switch.js
@@ -1,20 +1,3 @@
 document.getElementById("go-back").addEventListener("click" ,() =>{
     history.back();
 })
-const allSections = document.querySelector('.player-role');
-const sectBtn = document.querySelectorAll('.navigation');
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.category');
-
-
-function pageTransitions(){
-    //button click active class
-
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn'); //here, the current button has a class name of active-btn
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', ''); //once I click on a different button, the class 'active-btn' is replaced with an empty string 
-            this.className += 'active-btn';
-        })
-    }
-}
