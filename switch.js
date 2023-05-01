33521413d9540eb26233663507be03041b6fa18b
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-02 00:55:33 +0100

    changes made_a lot

--- switch.js
+++ switch.js
@@ -1,3 +1,9 @@
 document.getElementById("go-back").addEventListener("click" ,() =>{
     history.back();
 })
+
+const playerNavButton = document.getElementsById("navigation");
+
+playerNavButton.addEventListener("click", () => {
+    playerNavButton.
+})
